fe3bc6132cf8b417846027654215962dd2833e18
Maximiliano Romay Figueroa <maximilianorom7@gmail.com>
2017-10-03 15:11:58 -0300

    message function

--- buffers.el
+++ buffers.el
@@ -1,6 +1,6 @@
 (provide 'm-buffers)
 
-(defun buffer-unblock-block(buffer-name function args)
+(defun buffer-unblock-block(buffer-name function &optional args)
   "disable buffer read only if it is and executes the function
 after execution makes buffer read only"
   (get-buffer-create buffer-name)
@@ -10,11 +10,20 @@ after execution makes buffer read only"
     (if buffer-read-only nil (toggle-read-only))
     ))
 
-(defun wmessage(message)
-  (buffer-unblock-block
-   "*Messages*"
-   (lambda()
-     (cl-loop for line in (split-string message "\n") do
-	      (message line)
-	      (insert (concat "\nMESSAGE: " line))))
-   nil))
+(defun loop-lines(lines func)
+  (cl-loop for line in (split-string lines "\n") do
+	   (funcall func line)))
+
+(defun lines-insert-margin(lines &optional margin)
+  (let ((margin (or margin "")))
+    (loop-lines lines
+		(lambda(line)
+		  (insert (concat "\n" margin line)))))
+  (insert "\n"))
+
+(defun wmessage(message &optional margin)
+  (let ((margin (or margin "MESSAGE: ")))
+    (buffer-unblock-block
+     "*Messages*"
+     (lambda()
+       (lines-insert-margin message margin)))))
