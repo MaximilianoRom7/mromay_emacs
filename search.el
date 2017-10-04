26fcb8dae802c16e7b559080e8b2cdcc8c29ce5b
Maximiliano Romay Figueroa <maximilianorom7@gmail.com>
2017-10-04 10:45:11 -0300

    new function to read-buffer

--- search.el
+++ search.el
@@ -1,4 +1,9 @@
 
+;; To let enter a space in minibuffer when calling read-buffer
+;; (define-key minibuffer-local-completion-map "\M- "
+;;   (lambda () (interactive) (insert " ")))
+
 (defun search-in-all-buffers()
   (interactive)
-  (multi-occur-in-matching-buffers "" (read-buffer "Search for ?  ")))
+  (let ((search-for (read-buffer "Search for ?  ")))
+    (multi-occur-in-matching-buffers "" search-for)))
